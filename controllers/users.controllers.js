910614543acdb4dc326870264a0cadfad0be0269
tapadar13 <monsurismyname@gmail.com>
2023-05-27 10:19:50 +0530

    validate search routes

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -16,6 +16,34 @@ const getUsersByUuid = (req, res) => {
 
 const searchUsersByQuery = (req, res) => {
   const { gender, age } = req.query;
+  if (!age && !gender) {
+    return res.status(422).json({
+      message: "Missing Search Parameters, search using age and/or gender",
+    });
+  }
+
+  if (age) {
+    if (!Number(age)) {
+      return res
+        .status(422)
+        .json({ message: "Age parameter should be a number" });
+    }
+
+    if (age >= 100 || age < 0) {
+      return res.status(422).json({
+        message: "Age out of bounds. It should be a number between 0 and 100",
+      });
+    }
+  }
+
+  if (gender) {
+    if (!["female", "male"].includes(gender)) {
+      return res
+        .status(422)
+        .json({ message: "Gender to search can either be 'male' or 'female'" });
+    }
+  }
+
   if (gender && age) {
     const result = data.filter(
       (item) => item.gender === gender && Number(item.dob.age) >= Number(age)
