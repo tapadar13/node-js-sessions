910d8df6d315246a7a66ddf5ea09708b4c776c65
tapadar13 <monsurismyname@gmail.com>
2023-06-07 10:16:39 +0530

    refactored

--- models/blogs.model.js
+++ models/blogs.model.js
@@ -1,11 +1,14 @@
 const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-  title: { type: String, required: true, unique: true },
-  author: { type: [String] },
-  content: { type: String, default: "" },
-  publishedAt: { type: Date, default: null },
-});
+const blogSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, unique: true },
+    author: { type: [String] },
+    content: { type: String, default: "" },
+    publishedAt: { type: Date, default: null },
+  },
+  { timestamps: true }
+);
 
 const blogModel = mongoose.model("Blogs", blogSchema);
 
