77f921c9db3125430ca960741442fa5c67bda205
tapadar13 <monsurismyname@gmail.com>
2023-06-07 12:18:58 +0530

    create authorSchema

--- models/blogs.model.js
+++ models/blogs.model.js
@@ -1,9 +1,16 @@
 const mongoose = require("mongoose");
 
+const authorSchema = new mongoose.Schema({
+  fullName: { type: String, maxLength: 25 },
+  twitterHandle: { type: String },
+  email: { type: String, requied: true, maxLength: 50 },
+  image: { type: String },
+});
+
 const blogSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, unique: true },
-    author: { type: [String] },
+    author: [authorSchema],
     content: { type: String, default: "" },
     publishedAt: { type: Date, default: null },
   },
