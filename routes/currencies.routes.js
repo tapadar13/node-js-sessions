452c72d7834e6c656f7e7629187e25e03306c09b
tapadar13 <monsurismyname@gmail.com>
2023-07-02 22:35:47 +0530

    refactored

--- routes/currencies.routes.js
+++ routes/currencies.routes.js
@@ -1,12 +1,10 @@
 const {
-  getCurrencyTitle,
   getCurrencies,
   getCurrenciesWithSymbol,
 } = require("../controllers/currencies.controller");
 
 const router = require("express").Router();
 
-// router.get("/", getCurrencyTitle);
 router.get("/", getCurrencies);
 router.get("/:symbol", getCurrenciesWithSymbol);
 
